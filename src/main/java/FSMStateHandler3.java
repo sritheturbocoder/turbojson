77ae9104c211e581c6e829fceb7bd388108c72b8
Srikanth <sr.ai@outlook.com>
2021-01-07 16:50:31 +0800

    finite state machine - state3

---
+++ src/main/java/FSMStateHandler3.java
@@ -0,0 +1,71 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class FSMStateHandler3 implements Callable<Boolean>, IFSMStateHandler {
+
+    private FSMContext ctx;
+    private ExecutorService executor;
+
+    public FSMStateHandler3(FSMContext ctx, ExecutorService executor) {
+        this.ctx = ctx;
+        this.executor = executor;
+    }
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Boolean call() throws Exception {
+        return this.state3();
+    }
+
+    private boolean state3()
+    {
+        while (ctx.L.getChar()) {
+            if (ctx.L.getInputChar() >= '0' && ctx.L.getInputChar() <= '9') {
+                ctx.L.getStringBuilder().append((char) ctx.L.getInputChar());
+                continue;
+            }
+
+            if (ctx.L.getInputChar() == ' ' ||
+                    ctx.L.getInputChar() >= '\t' && ctx.L.getInputChar() <= '\r') {
+                ctx.Return = true;
+                ctx.NextState = 1;
+                return true;
+            }
+
+            switch (ctx.L.getInputChar()) {
+                case ',':
+                case ']':
+                case '}':
+                    ctx.L.ungetChar();
+                    ctx.Return = true;
+                    ctx.NextState = 1;
+                    return true;
+
+                case '.':
+                    ctx.L.getStringBuilder().append((char) ctx.L.getInputChar());
+                    ctx.NextState = 5;
+                    return true;
+
+                case 'e':
+                case 'E':
+                    ctx.L.getStringBuilder().append((char) ctx.L.getInputChar());
+                    ctx.NextState = 7;
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Future<Boolean> registerHandler() {
+        return executor.submit(this);
+    }
+}
