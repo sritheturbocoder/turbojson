74253be0d67366c86b14a913f3364a0f1df3fa57
Srikanth <sr.ai@outlook.com>
2021-01-07 23:54:15 +0800

    finite state machine -state 13

---
+++ src/main/java/FSMStateHandler13.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class FSMStateHandler13 implements Callable<Boolean>, IFSMStateHandler {
+
+    private final FSMContext ctx;
+    private final ExecutorService executor;
+
+    public FSMStateHandler13(FSMContext ctx, ExecutorService executor) {
+        this.ctx = ctx;
+        this.executor = executor;
+    }
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Boolean call() throws Exception {
+        return this.state13();
+    }
+
+    private boolean state13 ()
+    {
+        ctx.L.getChar();
+
+        if (ctx.L.getInputChar() == 'l') {
+            ctx.nextState = 14;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Future<Boolean> registerHandler() {
+        return executor.submit(this);
+    }
+}
