760b7d222d0dfb69eb7991c96aefe00a85512ee4
Srikanth <sr.ai@outlook.com>
2021-01-08 16:00:11 +0800

    finite state machine -state 25

---
+++ src/main/java/FSMStateHandler25.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class FSMStateHandler25 implements Callable<Boolean>, IFSMStateHandler {
+
+    private FSMContext ctx;
+    private ExecutorService executor;
+
+    public FSMStateHandler25(FSMContext ctx, ExecutorService executor) {
+        this.ctx = ctx;
+        this.executor = executor;
+    }
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Boolean call() throws Exception {
+        return this.state25();
+    }
+
+    private boolean state25()
+    {
+        ctx.L.getChar();
+
+        switch (ctx.L.getInputChar()) {
+            case '*':
+                ctx.nextState = 27;
+                return true;
+
+            case '/':
+                ctx.nextState = 26;
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+
+
+    @Override
+    public Future<Boolean> registerHandler() {
+        return executor.submit(this);
+    }
+}
