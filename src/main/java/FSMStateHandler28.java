dcba68ca84d50fab872c7ea64f186aa7ea8095cc
Srikanth <sr.ai@outlook.com>
2021-01-08 19:53:05 +0800

    finite state machine -state 28

---
+++ src/main/java/FSMStateHandler28.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class FSMStateHandler28 implements Callable<Boolean>, IFSMStateHandler {
+
+    private final FSMContext ctx;
+    private final ExecutorService executor;
+
+    public FSMStateHandler28(FSMContext ctx, ExecutorService executor) {
+        this.ctx = ctx;
+        this.executor = executor;
+    }
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Boolean call() throws Exception {
+        return this.state28();
+    }
+
+    private boolean state28()
+    {
+        while (ctx.lexer.getChar()) {
+            if (ctx.lexer.getInputChar() == '*')
+                continue;
+
+            if (ctx.lexer.getInputChar() == '/') {
+                ctx.nextState = 1;
+                return true;
+            }
+
+            ctx.nextState = 27;
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Future<Boolean> registerHandler() {
+        return executor.submit(this);
+    }
+}
